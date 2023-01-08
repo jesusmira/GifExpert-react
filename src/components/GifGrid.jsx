a8935bcc60069a4cd2c927836a9da15eb4207053
Jesus <jesmira@hotmail.es>
2023-01-08 09:53:43 +0100

    Primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,33 @@
+import { GiffItem } from './GiffItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
+export const GifGrid = ({ category }) => {
+
+  const {images, isLoading} = useFetchGifs( category);
+
+
+    return (
+        <>
+            <h3>{category}</h3>
+            {
+              isLoading && (<h2>Cargando...</h2>)
+            }
+            
+
+            <div className='card-grid'>
+              
+              { images.map( ((img) =>(
+
+                 <GiffItem 
+                    key={img.id}
+                    {...img}
+                  />
+
+              ))
+
+              )}
+            </div>
+
+        </>
+  )
+}
